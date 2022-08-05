b515e1d8dd56db7bcdb355fd90cb281a85ebb641
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2022-08-05 11:33:17 -0300

    repaginacao do site

--- js/script.js
+++ js/script.js
@@ -14,22 +14,19 @@ const fetchPokemon = async (pokemon) => {
     }catch(error){
         console.log('Ocorreu um erro na solicitação!',error)
     }
-
 }
 
 const renderPokemon = async (pokemon) => {
     const pokemonObj = await fetchPokemon(pokemon.toLowerCase())
-    if(pokemonObj){
-        imgPokemon.style.opacity = 1;
+    if(pokemonObj && pokemonObj.name !== undefined){
         namePokemon.innerText = pokemonObj.name
         imgPokemon.src = pokemonObj['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
     }else{
         namePokemon.innerText = 'Not Found :('
-        imgPokemon.style.opacity = 0;
+        imgPokemon.src = '../imgs/pokeWord.png'
     }
     input.value = ''
 }
-
 
 btn.addEventListener('click',() => renderPokemon(input.value))
 window.addEventListener('keypress',(key) => {
@@ -37,3 +34,39 @@ window.addEventListener('keypress',(key) => {
         renderPokemon(input.value)
     }
 })
+
+imgPokemon.addEventListener('click',() => {
+    if(imgPokemon.className.includes('animation')){
+        imgPokemon.classList.remove('animation')
+    }else{
+        imgPokemon.classList.add('animation')
+    }
+
+})
+
+//Forma alternativa
+/* const fetchPokemon = async (poke) => {
+    try{
+        const url = `https://pokeapi.co/api/v2/pokemon/${poke}`
+        const response = await fetch(url)
+        const objPoke = await response.json() 
+        const name = objPoke.name
+        const img = objPoke['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        const pokeInfos = {name,img}
+        return pokeInfos
+    }catch{
+        namePokemon.innerText = 'Not Found :('
+    }
+}
+
+const renderPokemon = async (poke) => {
+    const pokeInfos = await fetchPokemon(poke.toLowerCase())
+    if(pokeInfos){
+        imgPokemon.style.opacity = 1
+        namePokemon.innerText = pokeInfos.name
+        imgPokemon.src = pokeInfos.img
+    }else{
+        imgPokemon.style.opacity = 0
+    }
+}
+ */
